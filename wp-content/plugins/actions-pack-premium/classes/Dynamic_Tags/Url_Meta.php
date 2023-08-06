40e9b400c3c721063825b2c95e1e02ce2aaabfdf
Mohammad Zero <67842065+m82ab@users.noreply.github.com>
2023-08-06 12:05:52 +0330

    Add files via upload

---
+++ wp-content/plugins/actions-pack-premium/classes/Dynamic_Tags/Url_Meta.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Actions_Pack\Dynamic_Tags;
+
+use ElementorPro\Modules\DynamicTags\Tags\Base\Data_Tag;
+use ElementorPro\Modules\DynamicTags\Module;
+
+
+Class Url_Meta extends Data_Tag {
+
+    public function get_name() {
+        return 'ap_url_meta';
+    }
+
+    public function get_title() {
+        return __( 'URL Meta', 'actions-pack' );
+    }
+
+    public function get_group() {
+        return [ Module::AUTHOR_GROUP ];
+    }
+
+    public function get_categories() {
+        return [ Module::URL_CATEGORY ];
+    }
+
+    public function register_controls() {
+        $this->add_control(
+            'key',
+            [
+                'label' => __( 'Meta Key', 'actions-pack' ),
+            ]
+        );
+    }
+
+    public function get_panel_template_setting_key() {
+        return 'key';
+    }
+
+    public function get_panel_template() {
+        return ' ({{{ key }}})';
+    }
+
+    public function get_value( array $options = [] ){
+
+        $url = '#';
+
+        if ( is_author() ) {
+            $key = $this->get_settings( 'key' );
+            if( !empty($key) ){
+                $url = get_user_meta( get_queried_object_id(), $key, true );
+            }
+        }
+
+        return $url;
+    }
+}
